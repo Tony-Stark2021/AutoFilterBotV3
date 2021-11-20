da8eeb977957a01a2ced13c392ba56c18f5fc870
goutham <82017907+Tony-Stark2021@users.noreply.github.com>
2021-11-21 01:24:56 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,12 +30,11 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [Ǥ o u τ ђ ค ϻ](https://t.me/sagar_alias_jacky)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Tutorial : [Click Me](https://t.me/sagar_alias_jacky)
 """
